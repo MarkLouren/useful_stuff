24840d313fb730abdd5d098464907907c666f754
Vladimir Klykov <MarkLouren@users.noreply.github.com>
2019-08-10 20:23:53 +0300

    Update 4-JavaScriptTips.js

--- 4-JavaScriptTips.js
+++ 4-JavaScriptTips.js
@@ -16,3 +16,23 @@ a = b //присваивание
 //
 let age = prompt('Сколько тебе лет?', 100);
 alert(`Тебе ${age} лет!`); // Тебе 100 лет!
+
+/*        IF / ELSE STATEMENTS  ?  */
+
+let accessAllowed;
+let age = prompt('Сколько вам лет?', '');
+
+if (age > 18) {
+  accessAllowed = true;
+} else {
+  accessAllowed = false;
+}
+// The same code: let accessAllowed = (age > 18) ? true : false;  => "?" - нужно назначить переменную в зависимости от условия.
+
+// оператор сравнения "age > 18" выполняется первым в любом случае
+// (нет необходимости заключать его в скобки)
+
+let accessAllowed = age > 18 ? true : false;
+
+// то же самое
+let accessAllowed = age > 18;
