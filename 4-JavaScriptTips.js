0cb91a403133802f9f145344637658c4bbf17bde
Vladimir Klykov <MarkLouren@users.noreply.github.com>
2019-08-18 15:22:10 +0300

    Update 4-JavaScriptTips.js

--- 4-JavaScriptTips.js
+++ 4-JavaScriptTips.js
@@ -261,3 +261,24 @@ welcome(); // теперь всё в порядке
 let double = n => n * 2;
 alert( double(3) ); // 6
 
+////////////////////////////////////////////////////////////////////////////
+/* SETS - FOR | OF*/
+////////////////////////////////////////////////////////////////////////////
+
+let set = new Set();
+
+let john = { name: "John" };
+let pete = { name: "Pete" };
+let mary = { name: "Mary" };
+
+// считаем гостей, некоторые приходят несколько раз
+set.add(john);
+set.add(pete);
+set.add(mary);
+set.add(john);
+set.add(mary);
+
+for (let user of set) {
+  console.log(user.name); // John (потом Pete и Mary)
+}
+
