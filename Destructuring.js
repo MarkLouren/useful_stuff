14fd206691a02b7d65db4861a2700fee925119c7
Vladimir Klykov <MarkLouren@users.noreply.github.com>
2019-12-04 22:38:16 +0200

    Update Destructuring.js

--- Destructuring.js
+++ Destructuring.js
@@ -26,3 +26,56 @@ height:200
 }
 Console.log(width);
 console.log (height);
+
+ //Option 5
+let a = 2;
+let b = 3;
+// перестановка деструктуризацией массива
+[a, b] = [b, a];
+console.log(a); // 3
+console.log(b); // 2
+
+// Function
+function topThree ([first, second, third]) {
+  return {
+    first: first,
+    second: second,
+    third: third
+  }
+}
+
+function sayMyName ({
+  firstName = 'Zell',
+  lastName = 'Liew'
+} = {}) {
+ console.log(firstName + ' ' + lastName);
+}
+
+
+// Reduction:
+
+const anObject = {
+  // ES6
+  aShorthandMethod (arg1, arg2) {},
+  // ES5
+  aLonghandMethod: function (arg1, arg2) {},
+}
+
+// Динамические свойства обьектов
+
+const newPropertyName = 'smile';
+// ES6
+const anObject = {
+  aProperty: 'a value',
+  // Динамические названия свойств
+  [newPropertyName]: ':D',
+  ['bigger ' + newPropertyName]: 'XD',
+}
+// Результат
+// {
+//   aProperty: 'a value',
+//   'bigger smile': 'XD'
+//   smile: ':D',
+// }
+
+
