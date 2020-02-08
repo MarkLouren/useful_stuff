7ea1445aa2daec9f084e2272df2206a2f8d8a733
Vladimir Klykov <MarkLouren@users.noreply.github.com>
2020-02-08 13:01:26 +0200

    Create 9-React.js

---
+++ 9-React.js
@@ -0,0 +1,7 @@
+/* CheetSheets
+- https://devhints.io/react
+- https://reactcheatsheet.com/
+- http://www.developer-cheatsheets.com/react */
+
+// new date
+   <h3>creation date: { (new Date()).toDateString()}</h3>
