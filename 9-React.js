62facb434f97d6df242c5608b6c182d7c5f42358
Vladimir Klykov <MarkLouren@users.noreply.github.com>
2020-02-17 23:55:44 +0200

    Update 9-React.js

--- 9-React.js
+++ 9-React.js
@@ -1,7 +1,9 @@
 /* CheetSheets
 - https://devhints.io/react
 - https://reactcheatsheet.com/
-- http://www.developer-cheatsheets.com/react */
+- http://www.developer-cheatsheets.com/react 
+- https://ru.reactjs.org/docs/hooks-reference.html 
+*/
 
 // new date
    <h3>creation date: { (new Date()).toDateString()}</h3>
